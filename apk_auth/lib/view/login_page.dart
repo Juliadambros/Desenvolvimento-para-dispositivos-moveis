0df6573f2ac5d2b2dfc2e18c79a36f5f01100217
Juliadambros <julia.dambros.ju@gmail.com>
2025-10-29 13:41:49 -0300

    atulização login

--- apk_auth/lib/view/login_page.dart
+++ apk_auth/lib/view/login_page.dart
@@ -4,81 +4,79 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
-  final userNameController = TextEditingController();
+  final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  void signUserIn(){}
-
+  void signUserIn() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
       body: SafeArea(
         child: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 50),
-              Icon(Icons.lock, size: 100),
-              SizedBox(height: 50),
-              Text(
-                'Seja bem Vindo',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 50),
+                Icon(Icons.lock, size: 100),
+                SizedBox(height: 50),
+                Text(
+                  'Seja bem vindo',
+                  style: TextStyle(color: Colors.black, fontSize: 35),
                 ),
-              ),
-              SizedBox(height: 25),
-              MyTextfield(
-                controller: userNameController,
-                hintText: "Email",
-                obscureText: false,
-              ),
-              SizedBox(height: 15),
-              MyTextfield(
-                controller: passwordController,
-                hintText: "Senha",
-                obscureText: true,
-              ),
-              SizedBox(height: 15),
-              Padding(
-                padding: EdgeInsetsGeometry.symmetric(horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                SizedBox(height: 25),
+                MyTextfield(
+                  controller: usernameController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15),
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: EdgeInsetsGeometry.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15),
+                MyButton(onTap: signUserIn, text: 'Entrar'),
+                SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      "Esqueceu sua senha?",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
+                      'Nao tem cadastro?',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                    SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'Registre-se Agora!',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 15,),
-              MyButton(onTap: signUserIn, text: "Entrar"),
-              SizedBox(height: 25,),
-
-
-
-              SizedBox(width: 5,),
-              GestureDetector(
-                onTap: () {},
-                child: Text(
-                  "Registre-se Agora",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
